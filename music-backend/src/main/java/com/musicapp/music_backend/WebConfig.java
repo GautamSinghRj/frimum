26eed434699ee637d724281afa8640ef271f0400
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-04-29 16:57:11 +0530

    Update WebConfig.java

--- music-backend/src/main/java/com/musicapp/music_backend/WebConfig.java
+++ music-backend/src/main/java/com/musicapp/music_backend/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://frimum.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
