90a78f27332c6da15f34ab842abb2027ec08244d
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-05-13 22:19:24 +0530

    fix: update Songs component to use motion for h1 element for improved animations

--- src/animatedRoutes.jsx
+++ src/animatedRoutes.jsx
@@ -27,7 +27,7 @@ const PageWrapper=({children})=>{
     initial={{ opacity: 0, y: 30 }}
     animate={{ opacity: 1, y: 0 }}
     exit={{ opacity: 0, y: -30 }}
-    transition={{ duration: 0.3, ease: "circOut" }}
+    transition={{ duration: 0.3 }}
        >
     {children}
    </motion.div>;
--- src/songs.jsx
+++ src/songs.jsx
@@ -3,13 +3,14 @@ import Footer from "./footer";
 import {MoodCard} from './mood.jsx'
 import {ArtistCard} from './artist.jsx'
 import { moods, artists } from './data.js';
+import { motion } from "framer-motion";
 
 function Songs(){
     return(
         <div>
           <Header />
           <main>
-          <h1 className="w-full font-bebas font-black translate-x-16 transform text-4xl md:text-4xl xl:text-4xl">Find Your Mood!</h1>
+          <motion.h1 whileHover={{textDecoration:underline, translateX: '16px' }}  className="w-full font-bebas font-black translate-x-16 transform text-4xl md:text-4xl xl:text-4xl">Find Your Mood!</motion.h1>
           <div className="p-10 flex flex-col md:flex-row xl:flex-row justify-center items-center gap-10 md:gap-20 xl:gap-28">
             {moods.map((mood) => (
               <div key={mood.id} >
