68ddd0be0467bca7cd48aff0c9e61db73b5e8400
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-05-14 17:20:16 +0530

    fix: adjust search button styling and textarea height for consistency

--- src/header.jsx
+++ src/header.jsx
@@ -87,13 +87,13 @@ const stripePromise = loadStripe(paykey);
                 <Link to="/srcsong">
                 <img
                     loading="lazy"
-                    className="relative w-auto h-12 translate-x-6 transform "
+                    className="relative w-auto h-12"
                     src="./pic/search.png"
                     alt="Search button"
                 />
                 </Link>
                      <textarea
-                    className="!bg-slate-400 !text-slate-50 resize-none text-center rounded-full truncate font-medium text-xl border-none w-80 h-12 py-2"
+                    className="resize-none text-center rounded-full truncate font-medium text-xl border-none w-80 h-10 py-2 -translate-x-6 transform"
                     name="search"
                     id="search"
                     rows={3}
