146e468892e7c7190b9375e791de637baabab789
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-05-15 21:26:39 +0530

    fix: adjust textarea position for improved layout in Header component

--- src/header.jsx
+++ src/header.jsx
@@ -93,7 +93,8 @@ const stripePromise = loadStripe(paykey);
                 </Link>
                      <textarea
                     style={{ backgroundColor: 'rgb(107, 114, 128)', color: 'white' }}
-                    className="placeholder-white resize-none text-center rounded-full truncate font-medium text-xl border-none w-80 h-10 py-2 transform -translate-x-[6rem]"
+                    className="placeholder-white resize-none text-center rounded-full truncate font-medium text-xl border-none 
+                    w-80 h-10 py-2 transform -translate-x-[15rem]"
                     name="search"
                     id="search"
                     rows={3}
