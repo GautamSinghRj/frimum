98989e68693194af5997d2e20bddccc5adf82e3a
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-05-15 11:19:23 +0530

    fix: update header positioning and add overflow handling to main section for improved layout

--- src/songs.jsx
+++ src/songs.jsx
@@ -9,7 +9,7 @@ function Songs(){
     return(
         <div>
           <Header />
-          <main>
+          <main className="overflow-y-auto">
           <h1 className="w-full font-bebas font-black translate-x-16 transform text-4xl 
           md:text-4xl xl:text-4xl hover:underline transform transition-all duration-300 ease-in-out">Find Your Mood!</h1>
           <div className="p-10 flex flex-col md:flex-row xl:flex-row justify-center items-center gap-10 md:gap-20 xl:gap-28">
