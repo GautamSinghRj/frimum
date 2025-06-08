5889262ba7d940b0b676930468c9fe94f4fdaa57
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-06-08 13:49:18 +0530

    Update auth.jsx

--- src/auth.jsx
+++ src/auth.jsx
@@ -169,6 +169,7 @@ const Auth = () => {
                     className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm black focus:outline-none focus:ring-blue-500 focus:border-blue-500 disabled:bg-gray-100 disabled:cursor-not-allowed"
                   />
                 </div>
+              </>
             )}
             
             <button 
