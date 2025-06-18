735e25262ba97c95e2bf1ab1bc23de7e4990b67c
GautamSinghRj <gautamadityasingh1@gmail.com>
2025-06-18 12:37:58 +0530

    Update auth.jsx

--- src/auth.jsx
+++ src/auth.jsx
@@ -87,7 +87,6 @@ const Auth = () => {
     setSuccess('');
   };
 
-  // If user is logged in, show logout interface
   if (logged) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
